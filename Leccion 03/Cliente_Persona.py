38e704629baf3c0eca04f7fc0a9b6a80f91977a5
Jesus <jesus.suarez1@ulatina.net>
2023-03-09 18:41:06 -0400

    Clase Persona y Cliente Persona

--- Leccion 03/Cliente_Persona.py
+++ Leccion 03/Cliente_Persona.py
@@ -1,4 +1,7 @@
 from Persona import *
 
-persona1 = Persona('Juan',28)
+persona1 = Persona('Juan', 28)
 print(persona1)
+
+empleado1 = Empleado('Jesus', 23, 10000)
+print(empleado1)
--- Leccion 03/Persona.py
+++ Leccion 03/Persona.py
@@ -1,5 +1,5 @@
 class Persona:
-    def __init__(self,nombre,edad):
+    def __init__(self, nombre, edad):
         self._nombre = nombre
         self._edad = edad
 
@@ -8,7 +8,7 @@ class Persona:
         return self._nombre
 
     @nombre.setter
-    def nombre(self,nombre):
+    def nombre(self, nombre):
         self._nombre = nombre
 
     @property
@@ -16,13 +16,16 @@ class Persona:
         return self._edad
 
     @edad.setter
-    def edad(self,edad):
+    def edad(self, edad):
         self._edad = edad
 
     def __str__(self):
-        return f'Persona: {self.nombre} {self.edad}'
+        return f'Persona-> Nombre: {self.nombre}, Edad: {self.edad}'
 
 class Empleado(Persona):
-    def __init__(self,nombre,edad,sueldo):  #Atributos de la clase "Persona"
-        super(). __init__(nombre,edad)      #Metodo super -> para poder acceder a los atributos de la clase Persona
+    def __init__(self, nombre, edad, sueldo):  # Atributos de la clase "Persona"
+        super().__init__(nombre, edad)  # Metodo super -> para poder acceder a los atributos de la clase Persona
         self.sueldo = sueldo
+
+    def __str__(self):
+        return f'Empleado: {self.sueldo} {super().__str__()}'
