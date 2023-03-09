8ba93f317d85c1082fb42c7def3e72b2a1f7b5fd
Jesus <jesus.suarez1@ulatina.net>
2023-03-09 18:17:03 -0400

    Clase herencia

---
+++ Leccion 03/Persona.py
@@ -0,0 +1,28 @@
+class Persona:
+    def __init__(self,nombre,edad):
+        self._nombre = nombre
+        self._edad = edad
+
+    @property
+    def nombre(self):
+        return self._nombre
+
+    @nombre.setter
+    def nombre(self,nombre):
+        self._nombre = nombre
+
+    @property
+    def edad(self):
+        return self._edad
+
+    @edad.setter
+    def edad(self,edad):
+        self._edad = edad
+
+    def __str__(self):
+        return f'Persona: {self.nombre} {self.edad}'
+
+class Empleado(Persona):
+    def __init__(self,nombre,edad,sueldo):  #Atributos de la clase "Persona"
+        super(). __init__(nombre,edad)      #Metodo super -> para poder acceder a los atributos de la clase Persona
+        self.sueldo = sueldo
