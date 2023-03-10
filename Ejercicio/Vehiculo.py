81609e9fa3db1fc0fc60f01eb37d4b87b947ef97
Jesus <jesus.suarez1@ulatina.net>
2023-03-09 22:58:39 -0400

    Eliminé los metodos 'Get' y 'Set'

--- Ejercicio/Vehiculo.py
+++ Ejercicio/Vehiculo.py
@@ -3,36 +3,12 @@ class Vehiculo:
         self.color = color
         self.ruedas = ruedas
 
-    @property
-    def color(self):
-        return self.color
-
-    @color.setter
-    def color(self, color):
-        self.color = color
-
-    @property
-    def ruedas(self):
-        return self.ruedas
-
-    @ruedas.setter
-    def ruedas(self, ruedas):
-        self.ruedas = ruedas
-
     def __str__(self):
         return f'Color: {self.color} , Ruedas: {str(self.ruedas)}'
 
 class Coche(Vehiculo):
     def __init__(self, color, ruedas, velocidad):
         super().__init__(color, ruedas)
-        self.velocidad = velocidad
-
-    @property
-    def velocidad(self):
-        return self.velocidad
-
-    @velocidad.setter
-    def velocidad(self, velocidad):
         self.velocidad = velocidad
 
     def __str__(self):
@@ -42,14 +18,6 @@ class Coche(Vehiculo):
 class Bicicleta(Vehiculo):
     def __init__(self, color, ruedas, tipo):
         super().__init__(color, ruedas)
-        self.tipo = tipo
-
-    @property
-    def tipo(self):
-        return self.tipo
-
-    @tipo.setter
-    def tipo(self, tipo):
         self.tipo = tipo
 
     def __str__(self):
