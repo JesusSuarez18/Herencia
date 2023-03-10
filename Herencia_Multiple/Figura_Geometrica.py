61798494edaef8ac68381699d32dc6d11598f4ca
Jesus <jesus.suarez1@ulatina.net>
2023-03-10 19:07:09 -0400

    Agregando validaciones para no tener errores

--- Herencia_Multiple/Figura_Geometrica.py
+++ Herencia_Multiple/Figura_Geometrica.py
@@ -1,7 +1,15 @@
 class Figura_Geometrica:
     def __init__(self, ancho, alto):
-        self._ancho = ancho
-        self._alto = alto
+        if self._validar_valor(ancho):
+            self._ancho = ancho
+        else:
+            self._ancho = 0
+            print(f'Valor fuera de rango: {ancho}')
+        if self._validar_valor(alto):
+            self._alto = alto
+        else:
+            self._alto = 0
+            print(f'Valor fuera de rango: {alto}')
 
     @property
     def ancho(self):
@@ -9,7 +17,11 @@ class Figura_Geometrica:
 
     @ancho.setter
     def ancho(self, ancho):
-        self._ancho = ancho
+        if self._validar_valor(ancho):
+            self._ancho = ancho
+        else:
+            self._ancho = 0
+            print(f'Valor fuera de rango: {ancho}')
 
     @property
     def alto(self):
@@ -17,7 +29,14 @@ class Figura_Geometrica:
 
     @alto.setter
     def alto(self, alto):
-        self._alto = alto
+        if self._validar_valor(alto):
+            self._alto = alto
+        else:
+            self._alto = 0
+            print(f'Valor fuera de rango: {alto}')
 
     def __str__(self):
         return f'Ancho: {self._ancho}, Alto: {self._alto}'
+
+    def _validar_valor(self, valor):
+        return True if 0 < valor < 10 else False
