af65cc370086be7d704ffbc688d504e646ffac10
Jesus <jesus.suarez1@ulatina.net>
2023-03-10 17:29:11 -0400

    Agregando titulo a los objetos

--- Herencia_Multiple/Prueba_figura_geometrica.py
+++ Herencia_Multiple/Prueba_figura_geometrica.py
@@ -1,7 +1,15 @@
 from Cuadrado import Cuadrado
+from Rectangulo import Rectangulo
 
 #MRO --> Method Resolution Order
 print(Cuadrado.mro())
 
-cuadrado1 = Cuadrado(5, 'Azul')
+print('Cuadrado'.center(30,"-"))
+cuadrado1 = Cuadrado(9, 'Azul')
 print(cuadrado1)
+print(f'Area del cuadrado: {cuadrado1.calcular_area()}')
+
+print('Rectangulo'.center(30,"-"))
+rectangulo1 = Rectangulo(9,4,'Verde')
+print(rectangulo1)
+print(f'Area del rectangulo: {rectangulo1.calcular_area()}')
