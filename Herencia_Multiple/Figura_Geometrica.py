c531897ed0ccb0b9ec365d7a2e84eca5d37e48e2
Jesus <jesus.suarez1@ulatina.net>
2023-03-11 12:29:08 -0400

    Agregando metodo abstractAgregando metodo abstractoo

--- Herencia_Multiple/Figura_Geometrica.py
+++ Herencia_Multiple/Figura_Geometrica.py
@@ -1,11 +1,18 @@
-class Figura_Geometrica:
+from abc import ABC, abstractmethod
+
+
+def _validar_valor(valor):
+    return True if 0 < valor < 10 else False
+
+
+class Figura_Geometrica(ABC):
     def __init__(self, ancho, alto):
-        if self._validar_valor(ancho):
+        if _validar_valor(ancho):
             self._ancho = ancho
         else:
             self._ancho = 0
             print(f'Valor fuera de rango: {ancho}')
-        if self._validar_valor(alto):
+        if _validar_valor(alto):
             self._alto = alto
         else:
             self._alto = 0
@@ -17,7 +24,7 @@ class Figura_Geometrica:
 
     @ancho.setter
     def ancho(self, ancho):
-        if self._validar_valor(ancho):
+        if _validar_valor(ancho):
             self._ancho = ancho
         else:
             self._ancho = 0
@@ -29,14 +36,15 @@ class Figura_Geometrica:
 
     @alto.setter
     def alto(self, alto):
-        if self._validar_valor(alto):
+        if _validar_valor(alto):
             self._alto = alto
         else:
             self._alto = 0
             print(f'Valor fuera de rango: {alto}')
 
+    @abstractmethod
+    def calcular_area(self):
+        pass
+
     def __str__(self):
         return f'Ancho: {self._ancho}, Alto: {self._alto}'
-
-    def _validar_valor(self, valor):
-        return True if 0 < valor < 10 else False
